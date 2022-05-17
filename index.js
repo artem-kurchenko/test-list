a7a850bdb35a77eae9505fab28b0dca183fb3106
Artem Kurchenko <meschken@yandex.ru>
2022-05-17 08:49:13 +0400

    add CRUD

--- index.js
+++ index.js
@@ -22,19 +22,38 @@ function renderList(data) {
 }
 
 function renderItem(value) {
-  let taskTemplate = `<li class="todo">
-   <p class="todo__text">
-       ${value}
-   </p>
-   <button class="button todo__btn todo__btn_type_edit">
-   </button>
-   <button class="todo__btn button todo__btn_type_copy">
-   </button>
-   <button class="todo__btn button todo__btn_type_remove"> 
-   </button>
-   </li>`; 
+  // let taskTemplate = `<li class="todo">
+  //  <p class="todo__text">
+  //      ${value}
+  //  </p>
+  //  <button class="button todo__btn todo__btn_type_edit">
+  //  </button>
+  //  <button class="todo__btn button todo__btn_type_copy">
+  //  </button>
+  //  <button class="todo__btn button todo__btn_type_remove"> 
+  //  </button>
+  //  </li>`; 
   todosListElement.insertAdjacentHTML('beforeend', taskTemplate)
-  
+  const taskTemplate = document.querySelector(".todo-template").content;
+  const taskNode = taskTemplate.cloneNode(true);
+  taskNode.querySelector(".todo__text").textContent = value;
+  taskNode.querySelector(".todo__btn_type_copy").addEventListener('click', (args) => {
+    let taskElement = args.target.closest(".todo").cloneNode(true);
+    todosListElement.appendChild(taskElement);
+
+  })
+  taskNode.querySelector(".todo__btn_type_remove").addEventListener('click', (args) => {
+    let taskElement = args.target.closest(".todo");
+    taskElement.remove();
+  })
+  taskNode.querySelector(".todo__btn_type_edit").addEventListener('click', (args) => {
+    let taskElement = args.target.closest('.todo');
+    let valueElement = taskElement.querySelector('.todo__text');
+    todosInputElement.value = valueElement.textContent;
+    todosFormElement.querySelector(".todos__submit-btn").textContent = "Сохранить";
+    editingItem = valueElement;
+  })
+  todosListElement.appendChild(taskNode)
 }
 
 
